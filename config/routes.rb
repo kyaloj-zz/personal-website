353c1c2e1a15b823361bfa2a964c369f517272e5
justus <jmbaluka@mfarm.co.ke>
2015-05-02 14:06:47 +0300

    added home folder for the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :homes
   resources :contacts
   resources :blogs
   resources :lives
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502105913) do
+ActiveRecord::Schema.define(version: 20150502110557) do
 
 # Could not dump table "abouts" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 20150502105913) do
     t.string   "results"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "lives", force: :cascade do |t|
